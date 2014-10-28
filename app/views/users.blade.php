909f738006721038b8dd32953451b3ed9447b878
r.mogi <r.mogi@edia.co.jp>
2014-10-28 11:34:09 +0000

    add text

--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -2,6 +2,7 @@
 @extends('common')
 
 @section('content')
+    <h2>User list</h2>
     @foreach ($users as $user)
         <p>ID {{ $user->id }} : {{ $user->name }} : {{ $user->created_at }}</p>
     @endforeach
